0307db56a9f50f980f8d013a96b56825f40b8912
KimLinhVu <klvbubble@gmail.com>
2022-09-12 15:18:36 -0700

    Added required user stories for Flix - Part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Kim-Linh Vu on 9/12/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
